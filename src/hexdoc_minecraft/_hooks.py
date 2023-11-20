4e65a949394698fafee03f36fcce8088db2fc4d6
object-Object <object@objectobject.ca>
2023-11-20 03:40:34 -0500

    Update with new hexdoc changes

--- src/hexdoc_minecraft/_hooks.py
+++ src/hexdoc_minecraft/_hooks.py
@@ -1,5 +1,9 @@
 from importlib.resources import Package
+from pathlib import Path
 
+from github import Github
+from hexdoc.core import ModResourceLoader
+from hexdoc.minecraft.assets import HexdocAssetLoader
 from hexdoc.plugin import (
     HookReturn,
     ModPlugin,
@@ -7,9 +11,13 @@ from hexdoc.plugin import (
     VersionedModPlugin,
     hookimpl,
 )
+from typing_extensions import override
 
 from .__gradle_version__ import FULL_VERSION, GRADLE_VERSION
 from .__version__ import PY_VERSION
+from .asset_loader import MinecraftAssetLoader
+from .minecraft_assets import MinecraftAssetsRepo
+from .properties import MinecraftProps
 
 
 class MinecraftPlugin(ModPluginImpl):
@@ -40,3 +48,25 @@ class MinecraftModPlugin(VersionedModPlugin):
         from hexdoc_minecraft._export import generated, resources
 
         return [generated, resources]
+
+    @override
+    def asset_loader(
+        self,
+        loader: ModResourceLoader,
+        *,
+        site_url: str,
+        asset_url: str,
+        render_dir: Path,
+    ) -> HexdocAssetLoader:
+        minecraft_props = MinecraftProps.model_validate(loader.props.extra["minecraft"])
+        return MinecraftAssetLoader(
+            loader=loader,
+            site_url=site_url,
+            asset_url=asset_url,
+            render_dir=render_dir,
+            repo=MinecraftAssetsRepo(
+                github=Github(),
+                ref=minecraft_props.ref,
+                version=minecraft_props.version,
+            ),
+        )
--- src/hexdoc_minecraft/cli/app.py
+++ src/hexdoc_minecraft/cli/app.py
@@ -2,32 +2,17 @@ import logging
 from pathlib import Path
 from zipfile import ZipFile
 
-from github import Github
-from hexdoc.cli.utils.args import (
-    DEFAULT_BRANCH,
-    PropsOption,
-    ReleaseOption,
-    VerbosityOption,
-)
-from hexdoc.cli.utils.load import export_metadata, load_common_data
+from hexdoc.cli.utils.args import PropsOption, VerbosityOption
 from hexdoc.cli.utils.logging import setup_logging
-from hexdoc.core import ModResourceLoader
-from hexdoc.minecraft import Tag
-from hexdoc.model import StripHiddenModel
+from hexdoc.core import Properties
 from typer import Typer
 
-from ..asset_loader import MinecraftAssetLoader
-from ..minecraft_assets import MinecraftAssetsRepo
 from ..piston_meta import VersionManifestV2
+from ..properties import MinecraftProps
 
 logger = logging.getLogger(__name__)
 
 CACHE_ROOT = Path(".hexdoc_minecraft")
-
-
-class MinecraftAssetsProps(StripHiddenModel):
-    ref: str
-    version: str
 
 
 app = Typer(
@@ -40,17 +25,20 @@ app = Typer(
 
 @app.command()
 def fetch(
-    version_id: str,
     *,
+    props_file: PropsOption,
     verbosity: VerbosityOption = 1,
 ):
     """Download the Minecraft client jar."""
     setup_logging(verbosity)
 
-    jar_path = CACHE_ROOT / version_id / "client.jar"
+    props = Properties.load(props_file)
+    minecraft_props = MinecraftProps.model_validate(props.extra["minecraft"])
+
+    jar_path = CACHE_ROOT / minecraft_props.version / "client.jar"
 
     manifest = VersionManifestV2.fetch()
-    package = manifest.fetch_package(version_id)
+    package = manifest.fetch_package(minecraft_props.version)
     package.downloads.client.fetch_file(jar_path)
 
     logger.info("Done.")
@@ -58,14 +46,17 @@ def fetch(
 
 @app.command()
 def unzip(
-    version_id: str,
     *,
+    props_file: PropsOption,
     verbosity: VerbosityOption = 1,
 ):
     """Partially extract the Minecraft client jar."""
     setup_logging(verbosity)
 
-    version_dir = CACHE_ROOT / version_id
+    props = Properties.load(props_file)
+    minecraft_props = MinecraftProps.model_validate(props.extra["minecraft"])
+
+    version_dir = CACHE_ROOT / minecraft_props.version
     jar_path = version_dir / "client.jar"
 
     with ZipFile(jar_path) as jar:
@@ -74,55 +65,13 @@ def unzip(
                 (
                     "assets/minecraft/blockstates/",
                     "assets/minecraft/models/",
-                    "assets/minecraft/textures/",
+                    "assets/minecraft/textures/block/",
+                    "assets/minecraft/textures/item/",
                 )
             ):
                 jar.extract(name, version_dir / "resources")
 
     logger.info("Done.")
-
-
-@app.command()
-def export(
-    version_id: str,  # TODO: props.extra this
-    *,
-    assets_ref: str = "master",  # TODO: props.extra that
-    branch: str = DEFAULT_BRANCH,
-    release: ReleaseOption = False,
-    props_file: PropsOption,
-    verbosity: VerbosityOption = 0,
-):
-    """Export all textures."""
-    # TODO: refactor so this isn't just duplicated from hexdoc export
-
-    props, pm, plugin = load_common_data(props_file, verbosity, branch)
-
-    loader = ModResourceLoader.clean_and_load_all(
-        props,
-        pm,
-        export=True,
-    )
-
-    site_path = plugin.site_path(versioned=release)
-    site_url = f"{loader.props.url}/{site_path.as_posix()}"
-
-    asset_loader = MinecraftAssetLoader(
-        loader=loader,
-        asset_url=props.env.asset_url,
-        site_url=site_url,
-        gaslighting_items=Tag.GASLIGHTING_ITEMS.load(loader).value_ids_set,
-        repo=MinecraftAssetsRepo(
-            github=Github(),
-            ref=assets_ref,
-            version=version_id,
-        ),
-    )
-
-    export_metadata(
-        loader,
-        asset_loader,
-        site_url,
-    )
 
 
 if __name__ == "__main__":
